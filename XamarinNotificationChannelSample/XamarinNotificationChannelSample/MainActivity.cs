c2afdb4dfb3f7377360de93d7faca1d450880b65
Isham Mohamed <isham4you@gmail.com>
2017-09-16 02:19:44 +0530

    Implemented

--- XamarinNotificationChannelSample/XamarinNotificationChannelSample/MainActivity.cs
+++ XamarinNotificationChannelSample/XamarinNotificationChannelSample/MainActivity.cs
@@ -7,12 +7,23 @@ namespace XamarinNotificationChannelSample
     [Activity(Label = "XamarinNotificationChannelSample", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private NotificationHelper myNoti;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button btnSendNotification = FindViewById<Button>(Resource.Id.buttonSendNotification);
+            btnSendNotification.Click += BtnSendNotification_Click;
+        }
+
+        private void BtnSendNotification_Click(object sender, System.EventArgs e)
+        {
+            var myNotification = myNoti.notification("Test", "Hey! This is a test notification, did you notice the red dot");
+            myNoti.notify(1000, myNotification);
         }
     }
 }
---
+++ XamarinNotificationChannelSample/XamarinNotificationChannelSample/NotificationHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Java.Lang;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinNotificationChannelSample
+{
+    class NotificationHelper
+    {
+        private NotificationManager nManager;
+        const string CHANNEL = "MyNotificationChannel";
+
+        public NotificationHelper(Context cntx)
+        {
+            NotificationChannel nChannel = new NotificationChannel(CHANNEL, "My First Channel", NotificationImportance.High);
+            nChannel.Group = "XAPP_CHANNEL_GROUP";
+            nChannel.EnableVibration(true);
+            nChannel.SetShowBadge(true);
+
+            AppnManager().CreateNotificationChannel(nChannel);
+        }
+
+        public Notification.Builder notification(string title, string text)
+        {
+            return new Notification.Builder(Application.Context, CHANNEL)
+                .SetContentTitle(title)
+                .SetContentText(text)
+                .SetGroup("XAPP_CHANNEL_GROUP")
+                .SetAutoCancel(true);
+        }
+
+        public void notify(int id, Notification.Builder notification)
+        {
+            AppnManager().Notify(id, notification.Build());
+        }
+
+        private NotificationManager AppnManager()
+        {
+            return (nManager == null ? nManager = (NotificationManager)Application.Context.GetSystemService(Context.NotificationService) : nManager);
+        }
+    }
+}
